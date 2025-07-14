0da01b3ccbbf613203c1d38d069da00583fce588
Eric Morales <eric.mor.labastida@gmail.com>
2025-07-13 23:20:52 -0600

    modificacion del hasrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter, BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-  <BrowserRouter basename='/fetch-music/'>
+  <HashRouter >
     <App />
 
-  </BrowserRouter>
+  </HashRouter>
 );
 
 
